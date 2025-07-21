7fa06bbbd261677bca4dcde1d3b6a62cd5e646f1
mitsuiakane <akane.sumitomo@gmail.com>
2025-07-21 21:41:07 +0900

    list

--- fruits.py
+++ fruits.py
@@ -35,3 +35,57 @@ for sc,scc in scores.items():
         print(f"80点以上は{sc}")
         
 
+
+numbers = [70, 80, 65, 90, 100]
+numm=len(numbers)
+print(numm)
+total=0
+for nuu in numbers:
+    total+=nuu
+pp=total/numm
+print(pp)
+
+word = "python"
+
+for w in word:
+    print(w)
+
+for r in range(1,10):
+    for t in range(1,10):
+        y=r*t
+        print(y)
+        
+        
+
+pulse_rate=[55,43,78,38,96,51]
+#print(pulse_rate[0])
+print(pulse_rate.index(55))
+for i in range(len(pulse_rate)):
+    print(f"{i+1}番目の猫の脈拍は{pulse_rate[i]}です")
+        
+max_pulse = max(pulse_rate)
+min_pulse= min(pulse_rate)
+max_index = pulse_rate.index(max_pulse)
+min_index=pulse_rate.index(min_pulse)
+print(f"一番脈拍が高いのは{max_index+1}番目の{max_pulse}です")
+print(f"一番脈拍が低いのは{min_index+1}番目の{min_pulse}です")
+total=[]
+for ii in pulse_rate:
+    if ii>=60:
+        #print(ii)
+        oo=pulse_rate.index(ii)
+        print(f"{oo+1}番目の猫({ii})")
+        total.append(ii)
+        #print(total)
+        uu=len(total)
+        #print(uu)
+print(f"60以上の猫は{uu}匹です")
+count=0
+for inu in pulse_rate:
+    count+=inu
+print(count)
+bb=len(pulse_rate)
+cc=count/bb
+cc=round(cc, 1)
+print(cc)
+
