6963451d57d0a4b025bc3d916b7aa66f8f236ff9
mitsuiakane <akane.sumitomo@gmail.com>
2025-09-01 21:24:23 +0900

    item

--- input.py
+++ input.py
@@ -21,3 +21,18 @@ if input_prin in part_names:
     print(f"第{input_prin}部「{part_names[input_prin]}」が好きなんですね！")
 else:
     print(f"こんにちは、{input_prin}部が好きなんですね！")
+
+items_imges = {
+    "剣" : "http://paiza.jp/learning/images/sword.png",
+    "盾" : "http://paiza.jp/learning/images/shield.png",
+    "回復薬" : "http://paiza.jp/learning/images/potion.png",
+    "クリスタル" : "http://paiza.jp/learning/images/crystal.png"
+}
+
+# アイテムの並び順リスト
+items_orders = ["剣", "盾", "回復薬", "クリスタル"]
+
+# ここから下を記述しよう
+for item_name in items_orders:
+    print( <img src='URL'><br>items_imges[item_name])
+    print(item_name)
